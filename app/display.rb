b59b11a7d4443b98f21ae5274cdc7b709bd8dd1f
Kehvarl <Kehvarl@Kehvarl.com>
2023-01-25 19:15:37 -0700

    framebuffer mostly in place

--- app/display.rb
+++ app/display.rb
@@ -1,36 +1,66 @@
 class Display
-  attr_accessor :border, :w, :h, :screen, :buff
   def initialize args
     @w = args.w || 64
     @h = args.h || 32
-    @margin = args.margin || 64
-    @screen = []
-    @sw = (1280 - (@margin*2))/@w
-    @sh = (720 - (@margin*2))/@h
-    (0..@w).each do |y|
-      @screen << []
-      (0..@h).each do |x|
-        @screen[y] << {
-    x: @sw * x + @margin,
-    y: @sh * y + @margin,
-    w: @sw,
-    h: @sh,
-    path: :pixel,
-    a: 255,
-    r: 0,
-    g: 0,
-    b: 0,
-    blendmode_enum: 1
+    @screen_width  = args.screen_width  || 1280
+    @screen_height = args.screen_heignt || 720
+    @margin_top    = args.margin_top    || 64
+    @margin_bottom = args.margin_bottom || 64
+    @margin_left   = args.margin_left   || 64
+    @margin_right  = args.margin_right  || 64
+    @sw = (@screen_width - (@margin_left + @margin_right))/@w
+    @sh = (@screen_height - (@margin_top + @margin_bottom))/@h
+    @screen_buffers = []
+    @current_buffer = 0
+    @screen_buffers << create_screen_buffer
+    @screen_buffers << create_screen_buffer
+  end
+
+  def create_screen_buffer
+    buffer = []
+    (0..@h).each do |y|
+      buffer << []
+      (0..@w).each do |x|
+        buffer[y] << {
+          x: (@sw * x) + @margin_left,
+          y: (@sh * y) + @margin_bottom,
+          w: @sw,
+          h: @sh,
+          path: :pixel,
+          a: 255,
+          r: 0,
+          g: 0,
+          b: 0,
+          blendmode_enum: 1
         }.sprite!
-      end     
+      end
     end
-    @buff = @screen.dup()
+    buffer
+  end
+
+  def setpixel x, y, r=0, g=0, b=0, buffer=0
+    @screen_buffers[buffer][y][x].r = r
+    @screen_buffers[buffer][y][x].g = g
+    @screen_buffers[buffer][y][x].b = b    
+  end
+
+  def next_buffer
+    if @current_buffer == 0
+      return 1
+    end
+    return 0
+  end
+
+  def next_screen_buffer
+    @screen_buffers[next_buffer]    
   end
 
   def swap
-    temp = @screen.dup
-    @screen = @buff.dup()
-    @buff = temp
+   @current_buffer = next_buffer
+  end
+
+  def screen
+    @screen_buffers[@current_buffer]
   end
   
  end
--- app/tick.rb
+++ app/tick.rb
@@ -1,5 +1,19 @@
-def tick args
-  args.state.display ||=  Display.new({})
+def initialize args
+  args.state.display ||=  Display.new({margin_bottom:256})
 
+
+  args.state.display.setpixel(rand(64), rand(32), 255, 0, 0, args.state.display.next_buffer)
+end
+
+def tick args
+  if args.state.tick_count == 0
+    initialize args
+  end
+  args.outputs.primitives << {x:0, y:0, w:1280, h:720, r:128, g:128, b:128}.solid! 
   args.outputs.primitives << args.state.display.screen
+
+  if args.inputs.keyboard.key_down.j
+    args.state.display.swap()
+  end
+  
 end
