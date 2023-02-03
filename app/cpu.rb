776f282dc23563700dabea7ded83800524c78d40
Kehvarl <Kehvarl@Kehvarl.com>
2023-02-02 20:25:08 -0700

    registers

--- app/cpu.rb
+++ app/cpu.rb
@@ -1,6 +1,17 @@
 class cpu
   def initialize args=nil
-    
+    @register = []
+    @i = 0
+    @pc = 0
+    @ram = []
+  end
+
+  def vf
+    @register[15]    
+  end
+
+  def vf=(arg)
+    @register[15] = arg
   end
   
 end
