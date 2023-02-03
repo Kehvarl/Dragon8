b9160b250b69d35c6b0c50782b022bd9c822d6dc
Kehvarl <Kehvarl@Kehvarl.com>
2023-02-02 20:56:56 -0700

    Start on opcode, clear display

--- app/cpu.rb
+++ app/cpu.rb
@@ -1,9 +1,13 @@
 class cpu
-  def initialize args=nil
+  def initialize display
+    @display = display
     @register = []
     @i = 0
     @pc = 0
-    @ram = []
+    @memory = []
+    (0..4096).each do
+      @memory << 0      
+    end
   end
 
   def vf
@@ -12,6 +16,33 @@ class cpu
 
   def vf=(arg)
     @register[15] = arg
+  end
+
+  def step
+    
+  end
+
+  def readbyte address
+    @memory[address].to_s(16).rjust(2,"0")
+  end
+
+  def fetch
+    opcode = readbyte(@pc) + readbyte(@pc + 1)
+    @pc += 2
+  end
+
+  def decode opcode
+    op = opcode[0]
+    case op
+    when "0"
+      if opcode[1] != "0"
+        # Execute Call Statment
+      elsif opcode = "00E0"
+        @display.clear()
+      elsif opcode = "00EE"
+        # Return from subroutine
+      end
+    end
   end
   
 end
