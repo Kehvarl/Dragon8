057304508b544c5e4168fbf4120c110ba4fef91d
Kehvarl <Kehvarl@Kehvarl.com>
2023-02-04 21:16:32 -0700

    implement call and return.  Implement skip

--- app/cpu.rb
+++ app/cpu.rb
@@ -8,6 +8,11 @@ class cpu
     (0..4096).each do
       @memory << 0      
     end
+    @sp = 0
+    @stack = []
+    (0..48).each do
+      @stack << 0
+    end
   end
 
   def vf
@@ -36,11 +41,26 @@ class cpu
     case op
     when "0"
       if opcode[1] != "0"
-        # Execute Call Statment
+      # Execute Call Statment
       elsif opcode = "00E0"
         @display.clear()
       elsif opcode = "00EE"
-        # Return from subroutine
+        @pc = @stack[@sp]
+        @sp -= 1
+      end
+    when "1" # Jump
+      address = opcode[1,3].to_i
+      @pc = address
+    when "2" # Call
+      @sp += 1
+      @stack[@sp] = @pc
+      address = opcode[1,3].to_i
+      @pc = address
+    when "3" # Skip Next If Register Equals
+      reg = opcode[1,1].to_i
+      val = opcode[2,2].to_i
+      if @register[reg] == val
+        @pc += 2
       end
     end
   end
