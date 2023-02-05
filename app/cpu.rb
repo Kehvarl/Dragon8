c63b63fbe2b3072eeede8e06102af0613b44b313
Kehvarl <Kehvarl@Kehvarl.com>
2023-02-05 12:31:21 -0700

    SE(Reg), and LD(Reg)

--- app/cpu.rb
+++ app/cpu.rb
@@ -40,28 +40,46 @@ class cpu
     op = opcode[0]
     case op
     when "0"
-      if opcode[1] != "0"
+      if opcode[1] != "0" # SYS NNN: Call System Routine NNN
       # Execute Call Statment
-      elsif opcode = "00E0"
+      elsif opcode = "00E0" # CLS: Clear Screen
         @display.clear()
-      elsif opcode = "00EE"
+      elsif opcode = "00EE" # RTN: Return from Subroutine
         @pc = @stack[@sp]
         @sp -= 1
       end
-    when "1" # Jump
+    when "1" # JMP NNN: Jump to address NNN
       address = opcode[1,3].to_i
       @pc = address
-    when "2" # Call
+    when "2" # CALL NNN: Go to subroutine at address NNN
       @sp += 1
       @stack[@sp] = @pc
       address = opcode[1,3].to_i
       @pc = address
-    when "3" # Skip Next If Register Equals
+    when "3" # SE Vx, kk: Skip Next If Register X Equals Value KK
       reg = opcode[1,1].to_i
       val = opcode[2,2].to_i
       if @register[reg] == val
         @pc += 2
       end
+    when "4" # SNE Vx, kk: kip Next If Register X Not Equals Value KK
+      reg = opcode[1,1].to_i
+      val = opcode[2,2].to_i
+      if @register[reg] != val
+        @pc += 2
+      end
+    when "5" # SE Vx, Vy: Skip Next If Register X Equals Register Y
+      regx = opcode[1,1].to_i
+      regy = opcode[2,1]/to_i
+      if @register[regx] == @register[regy]
+        @pc += 2
+      end
+    when "6" # LD Vx, kk: Load Value kk into Register X
+      reg = opcode[1,1].to_i
+      val = opcode[2,2].to_i
+      if @register[reg] != val
+        @pc += 2
+      end        
     end
   end
   
