9fb8738762439fe0b5ca902f25ccd3ef2d7fb785
Kehvarl <kehvarlkehvarl.com>
2023-06-05 19:03:19 -0700

    load filenames from disk

--- app/rompicker.rb
+++ app/rompicker.rb
@@ -9,6 +9,9 @@ attr_accessor :selected_file
   def render
     draw = []
     draw << {x: 256, y: 300, w: 512, h: 256, path: "sprites/RomPick.png"}.sprite!
+    (0..[@file_list.count, 6].min).each do |index|
+       draw << {x: 272, y:556 - (index * 16) -24, text: @file_list[index], r: 255, g: 255, b: 255}.label!
+     end
     draw
   end
   
