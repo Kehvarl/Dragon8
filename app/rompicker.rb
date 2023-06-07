c1d232cff8fcd69f18c4da04c3b60ee45b957eb9
Kehvarl <kehvarlkehvarl.com>
2023-06-06 19:17:51 -0700

    highlight selected ROM

--- app/rompicker.rb
+++ app/rompicker.rb
@@ -2,15 +2,22 @@ class RomPicker
 attr_accessor :selected_file
 
   def initialize args
+    @roms = []
     @file_list = args.gtk.list_files "data/roms/"
-  
+    @file_list.each do |attachment|
+      @roms << attachment if attachment[-4..-1]  == '.rom'
+    end
+    @selected = 0
   end
 
   def render
     draw = []
     draw << {x: 256, y: 300, w: 512, h: 256, path: "sprites/RomPick.png"}.sprite!
     (0..[@file_list.count, 6].min).each do |index|
-       draw << {x: 272, y:556 - (index * 16) -24, text: @file_list[index], r: 255, g: 255, b: 255}.label!
+      if index == @selected
+        draw << {x:270, y:556 - (index * 16) -48, w:484, h:24, r:128, g:0, b:128}.solid!
+      end
+      draw << {x:272, y:556 - (index * 16) -24, text:@roms[index], r:255, g:255, b:255}.label!
      end
     draw
   end
