3f2b09b51c7827e6a4fed5af36182f63e1db1fdb
Kehvarl <Kehvarl@Kehvarl.com>
2025-10-02 19:27:25 -0700

    sort of works

--- app/main.rb
+++ app/main.rb
@@ -1,8 +1,147 @@
-require 'app/rompicker.rb'
-require 'app/cpu.rb'
-require 'app/display.rb'
-require 'app/toggle_switch.rb'
-require 'app/momentary_switch.rb'
-require 'app/controls.rb'
-require 'app/keyboard.rb'
-require 'app/tick.rb'
+require 'app/ui/rompicker.rb'
+require 'app/vm/cpu.rb'
+require 'app/vm/display.rb'
+require 'app/ui/toggle_switch.rb'
+require 'app/ui/momentary_switch.rb'
+require 'app/ui/controls.rb'
+require 'app/vm/keyboard.rb'
+
+
+def initialize args
+  args.state.state = :main
+  args.state.run = false
+  args.state.display =  Display.new({margin_right: 128, margin_bottom:256})
+  args.state.controls = Controls.new()
+  args.state.rs = RunStop.new()
+  args.state.s = Step.new()
+  args.state.r = ROM_Load.new()
+  args.state.keyboard = Keyboard.new()
+  args.state.cpu = CPU.new(args.state.display)
+  contents = args.gtk.read_file "data/roms/drlogo.rom"
+  args.state.cpu.set(contents.to_s.unpack('n*'), 0x200)
+
+  args.state.rom = nil
+
+end
+
+def draw_console args
+  args.outputs.primitives << {x:0, y:0, w:1280, h:720, path:"sprites/console.png"}.sprite!
+  args.outputs.primitives << args.state.controls.render
+  args.outputs.primitives << args.state.display.screen
+  args.outputs.primitives << args.state.rs
+  args.outputs.primitives << args.state.s
+  args.outputs.primitives << args.state.r
+end
+
+def handle_keys args
+    args.inputs.keyboard.keys[:down].each do |key|
+    args.state.keyboard.keydown key
+  end
+
+  args.inputs.keyboard.keys[:up].each do |key|
+    args.state.keyboard.keyup key
+  end
+
+  if args.inputs.keyboard.key_down.p
+    args.state.r.click args
+    args.state.state = :rom_pick
+  end
+
+  if args.inputs.keyboard.key_down.r
+    #args.state.run = !args.state.run
+    args.state.rs.click(args)
+    args.state.state = :running
+  end
+
+  if args.inputs.keyboard.key_down.s
+    args.state.state = :step
+  end
+
+  if args.inputs.keyboard.key_down.q
+    $gtk.request_quit
+  end
+
+  if args.inputs.keyboard.key_down.b
+    args.state.display.clear
+    args.state.cpu.pc = 0x200
+    if args.state.state == :running
+      args.state.rs.click(args)
+    end
+    args.state.state = :initialize
+
+  end
+
+  if args.inputs.keyboard.key_down.m
+    args.state.cpu.debug = !args.state.cpu.debug
+  end
+end
+
+
+def main_tick args
+  args.state.rs.tick args
+  args.state.s.tick args
+  args.state.r.tick args
+
+  if args.state.rs.status == 1
+    args.state.state = :running
+  end
+
+  if args.state.s.status == 1
+    args.state.state = :step
+  end
+
+  if args.state.r.status == 1
+    args.state.state = :rom_pick
+  end
+end
+
+def rom_load_tick args
+  if args.state.rom == nil
+    args.state.rom = RomPicker.new(args)
+  end
+
+  args.state.rom.tick args
+
+  if args.state.rom.close_select
+      contents = args.gtk.read_file "data/roms/#{args.state.rom.selected_file}"
+
+      args.state.cpu.set(contents.to_s.unpack('n*'), 0x200)
+  end
+
+  if args.state.rom.close_quit or args.state.rom.close_select
+    args.state.rom = nil
+    args.state.state = :main
+  end
+end
+
+def tick args
+  if args.state.tick_count == 0
+    initialize args
+    args.state.state = :main
+  end
+
+  draw_console args
+
+  case args.state.state
+  when :initialize
+    initialize args
+
+  when :rom_pick
+    rom_load_tick args
+
+  when :running
+    args.state.cpu.tick args.state.keyboard
+    main_tick args
+    handle_keys args
+
+  when :step
+    args.state.cpu.tick args.state.keyboard
+    args.state.state = :main
+    handle_keys args
+
+  when :main
+    main_tick args
+    handle_keys args
+
+  end
+end
